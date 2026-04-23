e0df501ba5d7064f23a206e5a17720900e0fe725
Nicholas Balsom <NicholasBalsom@gmail.com>
2026-04-23 00:44:52 -0230

    Change motor pins

--- src/ROV_Arduino_Control_Code/ROV_Arduino_Control_Code.ino
+++ src/ROV_Arduino_Control_Code/ROV_Arduino_Control_Code.ino
@@ -1,36 +1,41 @@
 #include <Servo.h>
-#define MOTOR_FL 3 // Front Left Motor pin
-#define MOTOR_FR 5 // Front Right Motor pin
+#define MOTOR_FL 5 // Front Left Motor pin
+#define MOTOR_FR 11 // Front Right Motor pin
 #define MOTOR_BL 6 // Back Left Motor pin
 #define MOTOR_BR 9 // Back Right Motor pin
-#define MOTOR_UP1 10// Up Motor 1 pin
-#define MOTOR_UP2 11 // Up Motor 2 pin  
+#define MOTOR_UPL 3// Up Motor 1 pin
+#define MOTOR_UPR 10 // Up Motor 2 pin  
 
-Servo motorFL, motorFR, motorBL, motorBR, motorUp1, motorUp2;
+Servo motorFL, motorFR, motorBL, motorBR, motorUPL, motorUPR;
+
 // Variables to store motor values (1000-2000 for ESC control via Servo library)
 int motorSpeedFL = 1500;
 int motorSpeedFR = 1500;
 int motorSpeedBL = 1500;
 int motorSpeedBR = 1500;
-int motorSpeedUp1 = 1500;
-int motorSpeedUp2 = 1500;
+int motorSpeedUpL = 1500;
+int motorSpeedUpR = 1500;
+
 void setup() {
   Serial.begin(9600);
+
   // Attach ESCs to the correct pins
   motorFL.attach(MOTOR_FL);
   motorFR.attach(MOTOR_FR);
   motorBL.attach(MOTOR_BL);
   motorBR.attach(MOTOR_BR);
-  motorUp1.attach(MOTOR_UP1);
-  motorUp2.attach(MOTOR_UP2);
+  motorUpL.attach(MOTOR_UPL);
+  motorUpR.attach(MOTOR_UPR);
+
 // Set all motors to the neutral speed (1500 is neutral for most ESCs)
   motorFL.writeMicroseconds(1500);
   motorFR.writeMicroseconds(1500);
   motorBL.writeMicroseconds(1500);
   motorBR.writeMicroseconds(1500);
-  motorUp1.writeMicroseconds(1500);
-  motorUp2.writeMicroseconds(1500);
+  motorUpL.writeMicroseconds(1500);
+  motorUpR.writeMicroseconds(1500);
 }
+
 void loop () {
   if (Serial.available() > 0) {
     // Read the serial input from the laptop
@@ -49,7 +54,7 @@ void loop () {
    motorFR.writeMicroseconds(motorSpeedFR);
    motorBL.writeMicroseconds(motorSpeedBL);
    motorBR.writeMicroseconds(motorSpeedBR);
-   motorUp1.writeMicroseconds(motorSpeedUp1);
-   motorUp2.writeMicroseconds(motorSpeedUp2);
+   motorUPL.writeMicroseconds(motorSpeedUp1);
+   motorUPR.writeMicroseconds(motorSpeedUp2);
   }
 }
